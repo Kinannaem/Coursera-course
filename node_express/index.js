cca970e9b28e5295ffdbdfd5cfe5d1d7f76251e5
Kinan Naim <kinan.berlin@gmail.com>
2021-03-28 13:46:19 +0200

    Express serve Static Files

--- node_express/index.js
+++ node_express/index.js
@@ -1,14 +1,18 @@
 const express = require ('express')
 const http = require('http')
+const morgan = require('morgan')
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express ();
+app.use(morgan('dev')) // dev means development version which print additional information to the screen 
+
+app.use(express.static(__dirname+'/puplic'));
 
 app.use((req,res,next)=>{ // we use next to create middleware
-console.log(req.headers)
-res.statusCode=200;
+/* console.log(req.headers) we dont need console here because morgan will print the information
+ */res.statusCode=200;
 res.setHeader('Content-Type','text/html');
 res.end('<html><body><h1>This is an express Server</h1></body></html>')
 })
